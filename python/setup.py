5b71b7f75edbc16298623c59385ba0f55a4651d3
æriess <iseurie@gmail.com>
2021-09-13 18:17:51 -0400

    fix relative imports + restore original module name

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='lopqpy3',
+    name='lopq',
     version='1.0.0',
     description='yahoo lopq for product quantization',
     url='https://github.com/anas-899/lopq.git',
-    'author': 'Clayton Mellina,Yannis Kalantidis,Huy Nguyen',
-    'author_email': 'clayton@yahoo-inc.com',
-	'license': 'Apache-2.0',
+    author='Clayton Mellina,Yannis Kalantidis,Huy Nguyen',
+    author_email='clayton@yahoo-inc.com',
+    license='Apache-2.0',
     packages=[
-        'lopqpy3'
+        'lopq'
     ],
     install_requires=[
         'protobuf>=3.11',
